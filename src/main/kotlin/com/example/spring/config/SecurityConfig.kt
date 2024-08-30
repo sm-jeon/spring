9cce0d68a5e1a0c5e710087fbdd3ff663b8d759f
sm-jeon <137497948+sm-jeon@users.noreply.github.com>
2024-08-30 14:48:53 +0900

    [add] add SecurityConfig

---
+++ src/main/kotlin/com/example/spring/config/SecurityConfig.kt
@@ -0,0 +1,16 @@
+package com.example.spring.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@EnableWebSecurity
+@Configuration
+class SecurityConfig {
+    @Bean
+    fun securityFilterChain(httpSecurity: HttpSecurity): SecurityFilterChain {
+        return httpSecurity.csrf { it.disable() }.build()
+    }
+}
