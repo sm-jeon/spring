6e55b9032d43502465099e67e9fb8c35b67fd053
sm-jeon <137497948+sm-jeon@users.noreply.github.com>
2024-09-04 21:24:26 +0900

    [add] /post 하위 url에 대해 로그인 상태 확인

--- src/main/kotlin/com/example/spring/controller/PostController.kt
+++ src/main/kotlin/com/example/spring/controller/PostController.kt
@@ -1,4 +1,7 @@
 package com.example.spring.controller
 
+import org.springframework.web.bind.annotation.RestController
+
+@RestController("/post")
 class PostController {
 }
---
+++ src/main/kotlin/com/example/spring/filter/PostSessionFilter.kt
@@ -0,0 +1,31 @@
+package com.example.spring.filter
+
+import com.example.spring.entity.Session
+import com.example.spring.service.SessionService
+import jakarta.servlet.Filter
+import jakarta.servlet.FilterChain
+import jakarta.servlet.ServletRequest
+import jakarta.servlet.ServletResponse
+import jakarta.servlet.annotation.WebFilter
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
+import org.springframework.beans.factory.annotation.Autowired
+
+@WebFilter("/post/*")
+class PostSessionFilter @Autowired constructor(
+    val sessionService: SessionService,
+): Filter {
+
+    override fun doFilter(p0: ServletRequest?, p1: ServletResponse?, p2: FilterChain) {
+        val session = (p0 as HttpServletRequest).session
+        val sessionId = session.id
+        val userId = session.getAttribute("user_id") as String
+        val validate = userId.let { sessionService.validate(Session(sessionId, userId)) }
+        if(validate) {
+            sessionService.register(Session(sessionId, userId))
+            p2.doFilter(p0, p1)
+        } else {
+            (p1 as HttpServletResponse).sendError(401)
+        }
+    }
+}
